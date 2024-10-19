7c3b119563c2da3db8eb15fd16a4762365faede4
IGR2020 <159914009+IGR2020@users.noreply.github.com>
2024-10-19 10:09:42 +0530

    States. (Bug with 2nd color in list)

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def load(path: str, scale: int = 1) -> pg.Surface:
 def copy(image: pg.Surface) -> pg.Surface:
     return pg.transform.scale_by(image, 1)
 
+def groupColors(colors: list[tuple[int, int, int]]) -> list[list[tuple[int, int, int]]]:
+    groupedColors = []
+    for color in colors:
+        for gColors in groupedColors:
+            if abs(color[0] - gColors[0][0]) + abs(color[1] - gColors[0][1]) + abs(color[2] - gColors[0][2]) < 10:
+                gColors.append(color)
+                break
+        else:
+            groupedColors.append([color])
+    return groupedColors
+
 width, height = 900, 500
 window = pg.display.set_mode((width, height))
 pg.display.set_caption("War Simulator")
@@ -14,40 +25,45 @@ fps = 60
 clock = pg.time.Clock()
 run = True
 
+buildCost = 1800
+
 class Country:
-    def __init__(self, mask, rect) -> None:
-        self.mask = mask
-        self.rect = rect
-        self.army = []
-        self.factories = 1
-        self.factoryJobs = {"Building": 1}
-        self.buildProgress = []
-        self.buildTime = 100
+    def __init__(self, states) -> None:
+        self.jobs = {"Building": [{"State": 0, "Count": 1}]}
+        self.states: list[State] = states
+        self.buildSpeed = 0.1
+
+    def getTotalProduction(self):
+        return sum([state.factories for state in self.states])
+    
+    def getTotalEmployment(self):
+        return sum([build["Count"] for build in self.jobs["Building"]])
+    
+    def reduceJobs(self):
+        for build in self.jobs["Building"]:
+            if build["Count"] > 0: 
+                build["Count"] -= 1
+                return
 
     def script(self):
-        if sum(list(self.factoryJobs.values())) > self.factories:
-            for job in self.factryJobs:
-                if self.factoryJobs[job] > 0:
-                    self.factoryJobs[job] -= 1
-                    break
-        for i in range(self.factoryJobs["Building"]):
-            try:
-                self.buildProgress[i] += 0.01
-            except:
-                self.buildProgress.append(0)
-            if self.buildProgress[i] > self.buildTime:
-                self.factories += 1
-                self.buildProgress.pop(i)
+        if self.getTotalEmployment() > self.getTotalProduction():
+            self.reduceJobs()      
+        for build in self.jobs["Building"]:
+            self.states[build["State"]].buildProgress += build["Count"]*self.buildSpeed
+        for state in self.states:
+            state.script()
 
-    def select(self):
-        print("Total Factories")
-        print(self.factories)
-        print("Empolyment Status")
-        print(self.factoryJobs)
-        print("Build Status")
-        print(self.buildProgress)
-        print()
+class State:
+    def __init__(self, mask, rect) -> None:
+        self.factories = 1
+        self.mask = mask
+        self.rect = rect
+        self.buildProgress = 0
 
+    def script(self):
+        if self.buildProgress > buildCost:
+            self.factories += 1
+            self.buildProgress = 0
 
 
 def getColors(image: pg.Surface) -> set[tuple[int, int, int]]:
@@ -57,15 +73,18 @@ def getColors(image: pg.Surface) -> set[tuple[int, int, int]]:
             colors.add(tuple(image.get_at((i, j))))
     return colors
 
-def createMasks(image: pg.Surface, colors: set[tuple[int, int, int]]) -> dict[tuple[int, int, int], Country]:
+def createMasks(image: pg.Surface, colors: list[list[tuple[int, int, int]]], names: list[str]) -> dict[str, Country]:
     countries = {}
-    for color in colors:
-        temp = copy(image)
-        temp.set_colorkey(color)
-        mask = pg.mask.from_surface(temp)
-        mask.invert()
-        rect = pg.Rect(0, 0, image.get_width(), image.get_height())
-        countries[color] = Country(mask, rect)
+    for colorGroup, name in zip(colors, names):
+        states = []
+        for color in colorGroup:
+            temp = copy(image)
+            temp.set_colorkey(color)
+            mask = pg.mask.from_surface(temp)
+            mask.invert()
+            rect = pg.Rect(0, 0, image.get_width(), image.get_height())
+            states.append(State(mask, rect))
+        countries[name] = Country(states)
     return countries
 
 def collideMask(mask1, mask2, rect1, rect2) -> bool:
@@ -76,10 +95,12 @@ def collideMask(mask1, mask2, rect1, rect2) -> bool:
     return False
 
 world_map = load("fantasy.png", 4)
-countries = createMasks(world_map, getColors(world_map))
+groupedColors = groupColors(getColors(world_map))
+countryNames = ["India", "Russia", "Ameriaca", "Russia", "Germany", "Australia", "Argentina", "Brazil", "Denmark", "Italy", "Afghanistan", "South Africa"]
+countries = createMasks(world_map, groupedColors, countryNames)
 temp = pg.Surface((1, 1))
 temp.fill((255, 255, 255))
-point = Country(pg.mask.from_surface(temp), pg.Rect(0, 0, 1, 1))
+point = State(pg.mask.from_surface(temp), pg.Rect(0, 0, 1, 1))
 
 while run:
 
@@ -88,11 +109,11 @@ while run:
             run = False
 
         if event.type == pg.MOUSEBUTTONDOWN:
+            point.rect.topleft = pg.mouse.get_pos()
             for country in countries:
-                point.rect.topleft = pg.mouse.get_pos()
-                if collideMask(countries[country].mask, point.mask, countries[country].rect, point.rect):
-                    print("Country", country)
-                    countries[country].select()
+                for state in countries[country].states:
+                    if collideMask(state.mask, point.mask, state.rect, point.rect):
+                        print("Country", country)
 
     for country in countries:
         countries[country].script()
